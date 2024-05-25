ae2c4fc652ddfc2ff88036b0ca3c1220377027ec
jammie-commits <jay.mbugua.ph@gmail.com>
2024-05-25 18:20:10 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    sentences_with_exclamation = [sentence + '!' for sentence in sentence_list]    
+    return sentences_with_exclamation
+
+    
